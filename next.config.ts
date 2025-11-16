ac18a782447099f2e2cb2d0552e083363c871192
SkundKumar <skund.kr@gmail.com>
2025-11-16 20:38:23 +0530

    bf

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  experimental: {
     scrollRestoration: true,
   },
-  
 };
 
 export default nextConfig;
